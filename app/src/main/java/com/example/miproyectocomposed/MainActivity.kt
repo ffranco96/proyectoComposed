f21190082805d2511998430dc12f3e3dd0466ac9
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-10-09 14:56:13 -0300

    Configurando el estilo del menu

--- app/src/main/java/com/example/miproyectocomposed/MainActivity.kt
+++ app/src/main/java/com/example/miproyectocomposed/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.miproyectocomposed
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
@@ -18,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,7 +37,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyMenu()
+            MiProyectoComposedTheme(){
+                MyMenu()
+            }
         }
     }
 }
@@ -38,11 +47,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyMenu(){
     ProvideTextStyle(value = MaterialTheme.typography.bodySmall) {
-        Column {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(8.dp)) {
+            MyImage()
+            Spacer(modifier = Modifier.height(8.dp))
             MyButton("Sincronizar")
             Spacer(modifier = Modifier.height(16.dp))
             MyButton("Contar")
-            MyImage()
         }
     }
 }
@@ -52,8 +63,7 @@ fun MyButton(buttonText: String, modifier: Modifier = Modifier.padding(horizonta
     Button(onClick = { /*TODO*/ }) {
         Text(
             text = "$buttonText",
-            style = MaterialTheme.typography.bodyMedium,
-            color = Color.Green
+            style = MaterialTheme.typography.labelLarge
         )
     }
 }
@@ -61,10 +71,12 @@ fun MyButton(buttonText: String, modifier: Modifier = Modifier.padding(horizonta
 @Composable
 fun MyImage(){
     Image(painterResource(R.drawable.ic_launcher_foreground),
-    "Mi imagen")
+    "Mi imagen",
+    modifier = Modifier.size(30.dp).clip(CircleShape).background(MaterialTheme.colorScheme.onBackground))
 }
 
-@Preview
+//@Preview()
+@Preview (uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun MenuPreview() {
     MyMenu()
