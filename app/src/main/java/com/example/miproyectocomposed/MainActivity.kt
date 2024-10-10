a8978abe3df34472e23d727993dd805116c72104
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-10-10 10:19:16 -0300

    Utilizacion de LazyColumn para posible expansión del menu. Modificacion ancho de botones.

--- app/src/main/java/com/example/miproyectocomposed/MainActivity.kt
+++ app/src/main/java/com/example/miproyectocomposed/MainActivity.kt
@@ -7,33 +7,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.miproyectocomposed.ui.theme.MiProyectoComposedTheme
-import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,27 +43,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyMenu(options : List<String>){
-    ProvideTextStyle(value = MaterialTheme.typography.bodySmall) {
-        val scrollState = rememberScrollState()
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
+    Column( // Usamos Column para colocar la imagen arriba y las opciones abajo
         modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
-            .padding(8.dp)
-            .verticalScroll(scrollState)) {
-            MyImage()
-            options.forEach(){
-                Spacer(modifier = Modifier.height(8.dp))
-                MyButton(it)
-            }
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally // Centrar la imagen y las opciones horizontalmente
+    ) {
+    //ProvideTextStyle(value = MaterialTheme.typography.bodySmall) {
+        MyImage()
+        Spacer(modifier = Modifier.height(16.dp))
+        MyOptions(options)
+    }
+}
+
+// LazyColumn funcionara similar a nuestro RecyclerView, reciclando las vistas
+// y dibujando solo las que estan presentes en pantalla
+@Composable
+fun MyOptions(options : List<String>){
+    LazyColumn(modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
+        items(options){option ->
+            Spacer(modifier = Modifier.height(8.dp))
+            MyButton(option)
         }
     }
 }
 
 @Composable
 fun MyButton(buttonText: String, modifier: Modifier = Modifier.padding(horizontal = 20.dp)) {
-    Button(onClick = { /*TODO*/ }) {
+    Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth(0.8f)) {
         Text(
-            text = "$buttonText",
+            text = buttonText,
             style = MaterialTheme.typography.labelLarge
         )
     }
@@ -79,7 +87,7 @@ fun MyImage(){
     Image(painterResource(R.drawable.ic_launcher_foreground),
     "Mi imagen",
     modifier = Modifier
-        .size(30.dp)
+        .size(50.dp)
         .clip(CircleShape)
         .background(MaterialTheme.colorScheme.onBackground))
 }
@@ -88,7 +96,5 @@ fun MyImage(){
 @Preview (uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun MenuPreview() {
-    MyMenu(listOf("Sincronizar", "Contar", "Contar", "Contar", "Contar", "Contar", "Contar",
-        "Contar", "Contar", "Contar", "Contar", "Contar2", "Contar3", "Contar4", "Contar5",
-        "Contar6", "Contar7","Contar8", "Contar9"))
+    MyMenu(listOf("Sincronizar", "Contar"))
 }
